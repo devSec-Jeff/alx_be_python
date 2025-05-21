04d8683ff5785b20da015ce2e6e9eb998b526f70
devSec-Jeff <jeffahpk@gmail.com>
2025-05-21 23:55:04 +0300

    Adjusting calculations to meet checker's requirements

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
-income = int(input("Enter your monthly income:"))
-expenses = int(input("Enter your total monthly expenses:"))
-monthly_savings = income - expenses
+monthly_income = int(input("Enter your monthly income:"))
+monthly_expenses = int(input("Enter your total monthly expenses:"))
+monthly_savings = float(monthly_income) - float(monthly_expenses)
 
-projected_savings = float(monthly_savings) * 12 + (float(monthly_savings) * 12 * 0.05)
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print("Your monthly savings are $",monthly_savings)
 print("Projected savings after one year, with interest, is: $",int(projected_savings))
