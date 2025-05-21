ae236a637664b68fa2c3282a50af9a4ac8a644a2
devSec-Jeff <jeffahpk@gmail.com>
2025-05-21 23:49:50 +0300

    Converted monthly savings to float for calculation of projected savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,6 +2,7 @@ income = int(input("Enter your monthly income:"))
 expenses = int(input("Enter your total monthly expenses:"))
 monthly_savings = income - expenses
 
-projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+projected_savings = float(monthly_savings) * 12 + (float(monthly_savings) * 12 * 0.05)
+
 print("Your monthly savings are $",monthly_savings)
 print("Projected savings after one year, with interest, is: $",int(projected_savings))
