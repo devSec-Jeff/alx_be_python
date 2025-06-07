70096b73f4c51ed9786b447654d2290547f39793
Geoffrey Pkorkor <jeffahpk@gmail.com>
2025-06-07 19:16:38 +0300

    Added arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num1 == 0 or num2 == 0:
+                print("Error: Division by Zero")
+                return
+            return num1 / num2
+        case _:
+            print("Unknown operation entered. Please try again")
+
