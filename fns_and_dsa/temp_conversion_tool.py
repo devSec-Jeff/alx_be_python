794b9d60ac3bbbec1ab7c16925631810fbf4b3f1
Geoffrey Pkorkor <jeffahpk@gmail.com>
2025-06-07 21:14:31 +0300

    Added temp_conversion_tool.py

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,22 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius
+
+def convert_to_fahrenheit(celsius):
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return fahrenheit
+
+temp = int(input("Enter the temperature to convert:"))
+opt = input("Is this temperature in Celsius or Fahrenheit? (C/F):")
+
+if opt.lower() == 'c':
+    print(convert_to_fahrenheit(temp))
+elif opt.lower() == 'f':
+    print(convert_to_celsius(temp))
+else:
+    print("Option not determined")
