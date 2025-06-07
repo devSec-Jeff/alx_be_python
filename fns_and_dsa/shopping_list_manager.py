de68a3bbd2ae93a8ca08c5611312c14b3a24a789
Geoffrey Pkorkor <jeffahpk@gmail.com>
2025-06-07 19:58:36 +0300

    Made changes to meet checker requirements

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,7 +13,7 @@ def main():
         choice = input("Enter your choice:")
 
         if choice == '1':
-            item = input("Enter the Item to add:")
+            item = input("Enter the Item to add: ")
             shopping_list.append(item)
         elif choice == '2':
             item = input("Enter the Item to remove:")
