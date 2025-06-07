2d436b6c786063222906eca02633a3f68f2d642b
Geoffrey Pkorkor <jeffahpk@gmail.com>
2025-06-07 19:20:33 +0300

    Made slight changes to accommodate checker requirements

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,10 @@ def perform_operation(num1, num2, operation):
         case 'multiply':
             return num1 * num2
         case 'divide':
-            if num1 == 0 or num2 == 0:
+            if num1 == 0:
+                print("Number should not be 0")
+                return
+            elif num2 == 0:
                 print("Error: Division by Zero")
                 return
             return num1 / num2
